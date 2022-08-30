b326be8ff121d547ab7a102820f4adc82eb9453b
Christy Le <christylex3@gmail.com>
2022-08-30 00:35:36 -0400

    Added form on sign up page and form can extract user's input

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -40,4 +40,54 @@ const SignUp = () => {
             console.error(err);
         }
     };
-}
+
+    return (
+        <main>
+            {/* TODO: Style the login card! */}
+            <article className="login-card">
+                <div className="login-card-header">Sign Up</div>
+                <div className="login-card-body">
+                    {/* If there is data, return to homepage */}
+                    {data ? (<Link to="/"></Link>) : (
+                        <form onSubmit={handleFormSubmit}>
+                            <input 
+                                className="form-input"
+                                placeholder="Username"
+                                name="username"
+                                type="text"
+                                value={formState.username}
+                                onChange={handleChange}
+                            />
+                            <input 
+                                className="form-input"
+                                placeholder="Email"
+                                name="email"
+                                type="email"
+                                value={formState.email}
+                                onChange={handleChange}
+                            />
+                            <input 
+                                className="form-input"
+                                placeholder="Password"
+                                name="password"
+                                type="password"
+                                value={formState.password}
+                                onChange={handleChange}
+                            />
+                            {/* Hovering over submit button changes cursor to be pointer */}
+                            <button
+                                className="submit-btn"
+                                style={{ cursor: "pointer" }}
+                                type="submit"
+                            >
+                                Submit
+                            </button>
+                        </form>
+                    )}
+                </div>
+            </article>
+        </main>
+    );
+};
+
+export default SignUp;
