b848eaf5edf7a904981c99d88002eb1a64351627
Christy Le <christylex3@gmail.com>
2022-08-29 21:44:32 -0400

    Added basic userSchema with username, email, password, and savedComments

--- server/models/User.js
+++ server/models/User.js
@@ -1,2 +1,35 @@
 const { Schema, model } = require("mongoose");
-const bcrypt
+const bcrypt = require("bcrypt");
+
+// import schema from Comment.js
+const commentSchema = require("./Comment");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, "Please use a valid email address!"],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        savedComments: [commentSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
