154e5c1bd0fa64db3416e752e143be662583b726
MLyjak1 <mlyjak@gmail.com>
2022-08-30 14:13:34 -0400

    All Routes except Login Working in Navbar

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const About = () => {
-
+    return (
     <h1>About</h1>
-    
+    );
     };
     
     export default About;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Beginner = () => {
+const Home = () => {
     return(
-    <h1>Beginner</h1>
+    <h1>Home</h1>
     )
 };
 
