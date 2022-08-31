83ae4415c4d8bf2386b87bc6caf356d558ec1586
unknown <cody.stubbs@gmail.com>
2022-08-31 12:04:09 -0500

    started adding about content

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const About = () => {
     return (
-    <h1>About</h1>
+        <section>
+            <header class="flex justify-center">
+                <h1 class="">About</h1>
+            </header>
+
+            <main>
+                <p>
+                    Thanks for visiting our website! 
+                </p>
+                <p>
+                    Our goal at Level Up coding is to help people learn how to code at any level of experience!
+                </p>
+                <p>
+                    Feel free to navigate to the different course pages to watch whichever tutorial you are interested.
+                </p>
+                <p>
+                    Please look at the information below to contact one of the developers. Happy coding!
+                </p>
+            </main>
+        </section>
     );
-    };
+};
     
     export default About;
