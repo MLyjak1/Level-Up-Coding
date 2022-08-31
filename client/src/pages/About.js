c65a18e94a43a8514b2629c392dde97f39f47b1c
unknown <cody.stubbs@gmail.com>
2022-08-31 13:47:05 -0500

    added table of developers

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,32 +2,45 @@ import React from 'react';
 
 const About = () => {
     return (
-        <section className="aboutPage">
+        <main className="aboutPage">
             <title className="aboutTitle flex justify-center">
                 <h1 className="">About</h1>
             </title>
 
-            <main className="aboutContent flex flex-wrap justify-center">
-                <p className='break-after-column'>
+            <section className=" aboutContent container flex flex-wrap justify-center">
+                <p className="">
                     Thanks for visiting our website! 
                     Our goal at Level Up coding is to help people learn how to code at any level of experience! 
                     Feel free to navigate to the different course pages to watch whichever tutorial you are interested.
                     Please look at the information below to contact one of the developers. Happy coding!
                 </p>
-                {/* <br />
-                <p>
-                    Our goal at Level Up coding is to help people learn how to code at any level of experience!
-                </p>
-                <br />
-                <p>
-                    Feel free to navigate to the different course pages to watch whichever tutorial you are interested.
-                </p>
-                <br />
-                <p>
-                    Please look at the information below to contact one of the developers. Happy coding!
-                </p> */}
-            </main>
-        </section>
+            </section>
+            <section className="developers ">
+                <table className="">
+                    <tr>
+                        <th>Team</th>
+                        <th>Socials</th>
+                    </tr>
+                    <tr>
+                        <th>Dominic Fisher</th>
+                        <th><a href="https://github.com/DominicFisher18">https://github.com/DominicFisher18</a></th>
+                    </tr>
+                    <tr>
+                        <th>Christy Le</th>
+                        <th><a href="https://github.com/christylex3" >https://github.com/christylex3</a></th>
+                    </tr>
+                    <tr>
+                        <th>Matthew Lyjak</th>
+                        <th><a href="https://github.com/MLyjak1">https://github.com/MLyjak1</a></th>
+                    </tr>
+                    <tr>
+                        <th>Cody Stubblefield</th>
+                        <th><a href="https://github.com/Shrike6">https://github.com/Shrike6</a></th>
+                    </tr>
+
+                </table>
+            </section>
+        </main>
     );
 };
     
