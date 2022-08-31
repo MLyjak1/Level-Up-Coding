0a3b032da5fcf78d81cd7aede0b4f09b81d04a4f
Christy Le <christylex3@gmail.com>
2022-08-31 00:48:17 -0400

    Added conditional rendering for login/signup/profile

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from "../utils/auth";
 
 function Navbar () {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
     return (
-        <nav className='flex justify-center'>
-            <Link to = "/"> 
-                <h3>Home</h3>
-            </Link>
-            <Link to="/about"> 
-                <h3>About</h3>
-            </Link>
-            {/* <Link to="/beginner"> 
-                <h3>Beginner</h3>
-            </Link> */}
-            <Link to="/login"> 
-                <h3>Login</h3>
-            </Link>
-            <Link to="/profile"> 
-                <h3>Profile</h3>
-            </Link>
-        </nav>
-    )
+		<nav className="flex justify-center">
+			<Link to="/">
+				<h3>Home</h3>
+			</Link>
+			<Link to="/about">
+				<h3>About</h3>
+			</Link>
+			{Auth.loggedIn() ? (
+				<>
+					<Link to="/profile">Profile</Link>
+				</>
+			) : (
+				<>
+					<Link to="/login">Login</Link>
+					<Link to="/signup">Signup</Link>
+				</>
+			)}
+			{/* <Link to="/login">
+				<h3>Login</h3>
+			</Link>
+			<Link to="/profile">
+				<h3>Profile</h3>
+			</Link> */}
+		</nav>
+	);
 };
 
 export default Navbar;
