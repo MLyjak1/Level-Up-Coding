8bed02dae4082654fd04ae2cef23a6ff3fb1b0e5
MLyjak1 <mlyjak@gmail.com>
2022-08-31 14:55:15 -0400

    Commiting for push

--- client/src/pages/Beginner.js
+++ client/src/pages/Beginner.js
@@ -1,36 +1,40 @@
 import React from 'react';
 
 const Beginner = () => {
-return(
-    <body className='beginner-page'>
-        <div className='beginner-video-1'>
-            <source src='placeholder for video'
-                    type='placeholder'>
-            </source>
-            <p className='beginner-text-1'>
-                    Placeholder text, will come back and finish later. Will describe the first beginner video.
-            </p>
-        </div>
+        return (
+                <div className="box">
+                        <div className='panel'>
+                                <body className='beginner-page'>
+                                        <div className='beginner-video-1'>
+                                                <source src='placeholder for video'
+                                                        type='placeholder'>
+                                                </source>
+                                                <p className='beginner-text-1'>
+                                                        Placeholder text, will come back and finish later. Will describe the first beginner video.
+                                                </p>
+                                        </div>
 
-        <div className='beginner-video-2'>
-            <source src='placeholder for video'
-                    type='placeholder'>
-            </source>
-            <p className='beginner-text-2'>
-                    Placeholder text, will come back and finish later. Will describe the second beginner video.
-            </p>
-        </div>
+                                        <div className='beginner-video-2'>
+                                                <source src='placeholder for video'
+                                                        type='placeholder'>
+                                                </source>
+                                                <p className='beginner-text-2'>
+                                                        Placeholder text, will come back and finish later. Will describe the second beginner video.
+                                                </p>
+                                        </div>
 
-        <div className='beginner-video-3'>
-            <source src='placeholder for video'
-                    type='placeholder'>
-            </source>
-            <p className='beginner-text-3'>
-                    Placeholder text, will come back and finish later. Will describe the third beginner video.
-            </p>
-        </div>
-    </body>
-)
+                                        <div className='beginner-video-3'>
+                                                <source src='placeholder for video'
+                                                        type='placeholder'>
+                                                </source>
+                                                <p className='beginner-text-3'>
+                                                        Placeholder text, will come back and finish later. Will describe the third beginner video.
+                                                </p>
+                                        </div>
+                                </body>
+                        </div>
+                </div>
+        )
 };
 
 export default Beginner;
