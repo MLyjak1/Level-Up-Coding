5acc34daeb9e2e55874eeb0ed9d2c1c1ec3f032e
DomDom <dominicfisher182001@gmail.com>
2022-09-01 12:01:59 -0400

    home page/intermediate/advanced

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import videos from '../utils/videoSeeds.json';
+// import videos from '../utils/videoSeeds.json';
 import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_VIDEOS } from '../utils/queries';
 
+
+
+
+// const beginners = data?.videos || {};
+// if (loading) {
+//     return <div>Loading...</div>;
+// }
+
+// const { loading, data } = useQuery(QUERY_VIDEOS, {
+//     // pass URL parameter
+//     variables: { courseLevel: "advanced" },
+//   });
+
+// const advanced = data?.videos || {};
+// if (loading) {
+//     return <div>Loading...</div>;
+// }
+
+// const { loading, data } = useQuery(QUERY_VIDEOS, {
+//     // pass URL parameter
+//     variables: { courseLevel: "intermediate" },
+//   });
+
+// const intermediate = data?.videos || {};
+// if (loading) {
+//     return <div>Loading...</div>;
+// }
 const Home = () => {
     const navigate = useNavigate();
-    let beginner = videos.filter(obj=>obj.courseLevel=='beginner')[0];
-    let intermediate = videos.filter(obj=>obj.courseLevel=='intermediate')[0];
-    let advanced = videos.filter(obj=>obj.courseLevel=='advanced')[0];
+    const { loading, data } = useQuery(QUERY_VIDEOS);
+    const videos = data?.videos || [];
+    console.log(videos);
+    // let beginner = videos.filter(obj=>obj.courseLevel=='beginner')[0];
+    // let intermediate = videos.filter(obj=>obj.courseLevel=='intermediate')[0];
+    // let advanced = videos.filter(obj=>obj.courseLevel=='advanced')[0];
 
     const goToBeginnerPage = (event) => {
         navigate("/beginner");
@@ -22,7 +54,7 @@ const Home = () => {
         <section className='homePage'>
         <div className='beginner'>
             <h1 className='beginnerHeader'>Beginner Coding Tutorials</h1>
-                <embed src={beginner.videoLink}></embed>
+                <embed src={videos.videoLink}></embed>
                 <p className='beginner-text'>
                     Placeholder text, will come back and finish later. Will describe the 3 beginner videos.
                 </p>
@@ -34,7 +66,35 @@ const Home = () => {
                 </button>
         </div>
 
-        <div className='intermediate'>
+        {/* <div className='intermediate'>
+            <h1 className='intermediateHeader'>Intermediate Coding Tutorials</h1>
+                <embed src={intermediate.videoLink}></embed>
+                <p className='intermediate-text'>
+                    Placeholder text, will come back and finish later. Will describe the 3 intermediate videos.
+                </p>
+                <button className='intermediate-button'
+                        style={{ cursor: 'pointer' }}
+                        onClick={goToIntermediatePage}
+                >
+                    Intermediate Page
+                </button>
+        </div>
+
+        <div className='advanced'>
+            <h1 className='advancedHeader'>Advanced Coding Tutorials</h1>
+                <embed src={advanced.videoLink}></embed>
+                <p className='advanced-text'>
+                    Placeholder text, will come back and finish later. Will describe the 3 advanced videos.
+                </p>
+                <button className='advanced-button'
+                        style={{ cursor: 'pointer' }}
+                        onClick={goToAdvancedPage}
+                >
+                    Advanced Page
+                </button>
+        </div> */}
+
+        {/* <div className='intermediate'>
             <h1 className='intermediateHeader'>Intermediate Coding Tutorials</h1>
                 <iframe src=''></iframe>
                 <p className='intermediate-text'>
@@ -60,7 +120,7 @@ const Home = () => {
                 >
                     Advanced Page
                 </button>
-        </div>
+        </div> */}
         </section>
     )
 };
