8bebc4747a9201d9450696c8c4056ceb62fb4b61
Christy Le <christylex3@gmail.com>
2022-09-01 14:20:18 -0400

    Fixed beginner, intermediate, and advanced pages to have embedded videos show. Need to fix individual pages again

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,44 +1,13 @@
 import React from 'react';
-// import videos from '../utils/videoSeeds.json';
+import videos from "../utils/videoSeeds.json";
 import { useNavigate } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_VIDEOS } from '../utils/queries';
 
-
-
-
-// const beginners = data?.videos || {};
-// if (loading) {
-//     return <div>Loading...</div>;
-// }
-
-// const { loading, data } = useQuery(QUERY_VIDEOS, {
-//     // pass URL parameter
-//     variables: { courseLevel: "advanced" },
-//   });
-
-// const advanced = data?.videos || {};
-// if (loading) {
-//     return <div>Loading...</div>;
-// }
-
-// const { loading, data } = useQuery(QUERY_VIDEOS, {
-//     // pass URL parameter
-//     variables: { courseLevel: "intermediate" },
-//   });
-
-// const intermediate = data?.videos || {};
-// if (loading) {
-//     return <div>Loading...</div>;
-// }
 const Home = () => {
     const navigate = useNavigate();
-    const { loading, data } = useQuery(QUERY_VIDEOS);
-    const videos = data?.videos || [];
-    console.log(videos);
-    // let beginner = videos.filter(obj=>obj.courseLevel=='beginner')[0];
-    // let intermediate = videos.filter(obj=>obj.courseLevel=='intermediate')[0];
-    // let advanced = videos.filter(obj=>obj.courseLevel=='advanced')[0];
+    
+    let beginner = videos.filter(obj=>obj.courseLevel=='beginner')[0];
+    let intermediate = videos.filter(obj=>obj.courseLevel=='intermediate')[0];
+    let advanced = videos.filter(obj=>obj.courseLevel=='advanced')[0];
 
     const goToBeginnerPage = (event) => {
         navigate("/beginner");
@@ -49,12 +18,11 @@ const Home = () => {
     const goToAdvancedPage = (event) => {
         navigate("/advanced");
     };
-
     return(
         <section className='homePage'>
         <div className='beginner'>
             <h1 className='beginnerHeader'>Beginner Coding Tutorials</h1>
-                <embed src={videos.videoLink}></embed>
+                <embed src={beginner.videoLink}></embed>
                 <p className='beginner-text'>
                     Placeholder text, will come back and finish later. Will describe the 3 beginner videos.
                 </p>
@@ -65,10 +33,9 @@ const Home = () => {
                     Beginner Page
                 </button>
         </div>
-
-        {/* <div className='intermediate'>
+        <div className='intermediate'>
             <h1 className='intermediateHeader'>Intermediate Coding Tutorials</h1>
-                <embed src={intermediate.videoLink}></embed>
+            <embed src={intermediate.videoLink}></embed>
                 <p className='intermediate-text'>
                     Placeholder text, will come back and finish later. Will describe the 3 intermediate videos.
                 </p>
@@ -79,10 +46,9 @@ const Home = () => {
                     Intermediate Page
                 </button>
         </div>
-
         <div className='advanced'>
             <h1 className='advancedHeader'>Advanced Coding Tutorials</h1>
-                <embed src={advanced.videoLink}></embed>
+            <embed src={advanced.videoLink}></embed>
                 <p className='advanced-text'>
                     Placeholder text, will come back and finish later. Will describe the 3 advanced videos.
                 </p>
@@ -91,38 +57,192 @@ const Home = () => {
                         onClick={goToAdvancedPage}
                 >
                     Advanced Page
-                </button>
-        </div> */}
-
-        {/* <div className='intermediate'>
-            <h1 className='intermediateHeader'>Intermediate Coding Tutorials</h1>
-                <iframe src=''></iframe>
-                <p className='intermediate-text'>
-                    Placeholder text, will come back and finish later. Will describe the 3 intermediate videos.
-                </p>
-                <button className='intermediate-button'
-                        style={{ cursor: 'pointer' }}
-                        onClick={goToIntermediatePage}
-                >
-                    Intermediate Page 
                 </button>
         </div>
-
-        <div className='advanced'>
-            <h1 className='advancedHeader'>Advanced Coding Tutorials</h1>
-                <iframe src=''></iframe>
-                <p className='advanced-text'>
-                    Placeholder text, will come back and finish later. Will describe the 3 advanced videos.
-                </p>
-                <button className='advanced-button'
-                        style={{ cursor: 'pointer' }}
-                        onClick={goToAdvancedPage}
-                >
-                    Advanced Page
-                </button>
-        </div> */}
         </section>
     )
 };
-
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// // import videos from '../utils/videoSeeds.json';
+// import { useNavigate } from 'react-router-dom';
+// import { useQuery } from '@apollo/client';
+// import { QUERY_VIDEOS } from '../utils/queries';
+
+
+
+
+// // const beginners = data?.videos || {};
+// // if (loading) {
+// //     return <div>Loading...</div>;
+// // }
+
+// // const { loading, data } = useQuery(QUERY_VIDEOS, {
+// //     // pass URL parameter
+// //     variables: { courseLevel: "advanced" },
+// //   });
+
+// // const advanced = data?.videos || {};
+// // if (loading) {
+// //     return <div>Loading...</div>;
+// // }
+
+// // const { loading, data } = useQuery(QUERY_VIDEOS, {
+// //     // pass URL parameter
+// //     variables: { courseLevel: "intermediate" },
+// //   });
+
+// // const intermediate = data?.videos || {};
+// // if (loading) {
+// //     return <div>Loading...</div>;
+// // }
+// const Home = () => {
+//     const navigate = useNavigate();
+//     const { loading, data } = useQuery(QUERY_VIDEOS, {
+//         // pass URL parameter
+//         variables: { courseLevel: "beginner" },
+//     });
+
+//     const beginnerVideos = data?.videos || {};
+//     if (loading) {
+//         return <div>Loading...</div>;
+//     }
+//     console.log(data);
+//     // const videos = beginnerData?.videos || [];
+
+//     // console.log(videos);
+//     // let beginner = videos.filter(video=>video.courseLevel=='beginner')[0];
+//     // let intermediate = videos.filter(video=>video.courseLevel=='intermediate')[0];
+//     // let advanced = videos.filter(video=>video.courseLevel=='advanced')[0];
+
+//     const goToBeginnerPage = (event) => {
+//         navigate("/beginner");
+//     };
+//     const goToIntermediatePage = (event) => {
+//         navigate("/intermediate");
+//     };
+//     const goToAdvancedPage = (event) => {
+//         navigate("/advanced");
+//     };
+
+//     return(
+//         <section className='homePage'>
+//         <div className='beginner'>
+//             <h1 className='beginnerHeader'>Beginner Coding Tutorials</h1>
+//                 <embed src={beginnerVideos.videoLink}></embed>
+//                 <p className='beginner-text'>
+//                     Placeholder text, will come back and finish later. Will describe the 3 beginner videos.
+//                 </p>
+//                 <button className='beginner-button'
+//                         style={{ cursor: 'pointer' }}
+//                         onClick={goToBeginnerPage}
+//                 >
+//                     Beginner Page
+//                 </button>
+//         </div>
+
+//         {/* <div className='intermediate'>
+//             <h1 className='intermediateHeader'>Intermediate Coding Tutorials</h1>
+//                 <embed src={intermediate.videoLink}></embed>
+//                 <p className='intermediate-text'>
+//                     Placeholder text, will come back and finish later. Will describe the 3 intermediate videos.
+//                 </p>
+//                 <button className='intermediate-button'
+//                         style={{ cursor: 'pointer' }}
+//                         onClick={goToIntermediatePage}
+//                 >
+//                     Intermediate Page
+//                 </button>
+//         </div>
+
+//         <div className='advanced'>
+//             <h1 className='advancedHeader'>Advanced Coding Tutorials</h1>
+//                 <embed src={advanced.videoLink}></embed>
+//                 <p className='advanced-text'>
+//                     Placeholder text, will come back and finish later. Will describe the 3 advanced videos.
+//                 </p>
+//                 <button className='advanced-button'
+//                         style={{ cursor: 'pointer' }}
+//                         onClick={goToAdvancedPage}
+//                 >
+//                     Advanced Page
+//                 </button>
+//         </div> */}
+
+//         <div className='intermediate'>
+//             <h1 className='intermediateHeader'>Intermediate Coding Tutorials</h1>
+//                 <iframe src=''></iframe>
+//                 <p className='intermediate-text'>
+//                     Placeholder text, will come back and finish later. Will describe the 3 intermediate videos.
+//                 </p>
+//                 <button className='intermediate-button'
+//                         style={{ cursor: 'pointer' }}
+//                         onClick={goToIntermediatePage}
+//                 >
+//                     Intermediate Page 
+//                 </button>
+//         </div>
+
+//         <div className='advanced'>
+//             <h1 className='advancedHeader'>Advanced Coding Tutorials</h1>
+//                 <iframe src=''></iframe>
+//                 <p className='advanced-text'>
+//                     Placeholder text, will come back and finish later. Will describe the 3 advanced videos.
+//                 </p>
+//                 <button className='advanced-button'
+//                         style={{ cursor: 'pointer' }}
+//                         onClick={goToAdvancedPage}
+//                 >
+//                     Advanced Page
+//                 </button>
+//         </div>
+//         </section>
+//     )
+// };
+
+// export default Home;
