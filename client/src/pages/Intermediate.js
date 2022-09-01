76e55e29f5852ccffd5d430ab30ca2bfda00ba04
MLyjak1 <mlyjak@gmail.com>
2022-09-01 12:12:19 -0400

    updating main

--- client/src/pages/Intermediate.js
+++ client/src/pages/Intermediate.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import videos from '../utils/videoSeeds.json';
+import React from "react";
+import videos from "../utils/videoSeeds.json";
 
 const Intermediate = () => {
-        const intermediate = videos.filter(obj => obj.courseLevel == 'intermediate');
-        return (
-                <div className="box">
-                        <div className='panel'>
-                                <div className='intermediate-page'>
-                                        {
-                                                intermediate.map(obj =>
-                                                        <div key={obj._id} className='{obj.title}'>
-                                                                <embed src={obj.videoLink}>
-                                                                </embed>
-                                                                <p className='intermediate-text-1'>
-                                                                        {obj.description}
-                                                                </p>
-                                                        </div>)
-                                        }
-                                </div>
-                        </div>
-                </div>
-        )
+  const intermediate = videos.filter(
+    (obj) => obj.courseLevel == "intermediate",
+  );
+  return (
+    <div className="box">
+      <div className="panel">
+        <div className="intermediate-page">
+          {intermediate.map((obj) => (
+            <div key={obj._id} className="{obj.title}">
+              <embed src={obj.videoLink}></embed>
+              <p className="intermediate-text-1">{obj.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Intermediate;
